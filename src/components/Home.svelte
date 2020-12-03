7120f9aa2816758257afab9524862873c490fbaf
Hua Anh Minh <huaanhminh0412@gmail.com>
2020-12-03 13:16:40 +0700

    Add notification when no internet

--- src/components/Home.svelte
+++ src/components/Home.svelte
@@ -1,3 +1,48 @@
-<div>
-  
+<script>
+  import { onMount } from 'svelte';
+  import NoInternetToast from './NoInternetToast.svelte';
+
+  let width = window.innerWidth;
+  const onWindowResize = (e) => {
+    if (e.target.innerWidth !== width) {
+      width = e.target.innerWidth;
+    }
+  };
+
+  let showNoInternetToast = false;
+  const setShowNoInternetToast = (show=false) => {
+    showNoInternetToast = show;
+  };
+
+  onMount(() => {
+    window.onresize = onWindowResize;
+    window.ononline = () => showNoInternetToast = false;
+    window.onoffline = () => showNoInternetToast = true;
+  });
+</script>
+
+<style lang="scss">
+  @import '../styles/Home.scss';
+</style>
+
+<div class="container-sm home">
+  <div class="row home__row">
+
+  {#if width < 992}
+    <div class="col">
+      
+    </div>
+  {:else}
+    <div class="col-2"></div>
+    <div class="col-8">
+      
+    </div>
+    <div class="col-2"></div>
+  {/if}
+
+    <NoInternetToast
+      show={showNoInternetToast}
+      onHide={setShowNoInternetToast}
+    />
+  </div>
 </div>
---
+++ src/components/NoInternetToast.svelte
@@ -0,0 +1,33 @@
+<style lang="scss">
+  @import '../styles/NoInternetToast.scss';
+</style>
+
+<script>
+  export let onHide;
+  export let show
+</script>
+
+<div
+  class="no-internet toast {show ? 'show' : ''}"
+  role="alert"
+  aria-atomic="true"
+  aria-live="assertive"
+>
+  <div class="toast-header no-internet__header">
+    <strong class="mr-auto no-internet__header__title">
+      No internet connection
+    </strong>
+    <button
+      type="button"
+      class="ml-2 mb-1 close no-internet__header__close"
+      data-dismiss="toast"
+      aria-label="Close"
+      on:click={() => onHide(false)}
+    >
+      <span aria-hidden="true">&times;</span>
+    </button>
+  </div>
+  <div class="toast-body no-internet__body">
+    Check your internet connection again.
+  </div>
+</div>
